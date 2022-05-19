dfabea33a0456463698841571626863f81e079c8
John W. Terrell <john@coolpeoplenetworks.com>
2022-05-18 18:50:26 -0700

    Removed BSV cmake code in favor of new submodule

--- cmake/BSV/Compiler/BSVC-BSV.cmake
+++
@@ -1,4 +0,0 @@
-# This file sets the basic flags for the BSV compiler
-set(CMAKE_BSV_FLAGS_INIT "$ENV{BSVFLAGS} ${CMAKE_BSV_FLAGS_INIT}")
-
-cmake_initialize_per_config_variable(CMAKE_BSV_FLAGS "Flags used by the BSV compiler")
